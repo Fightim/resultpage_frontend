ee1d771c508eed89f435b6a2cd854ad553b48a85
EXAMPLE1ST <koo6250@naver.com>
2022-10-19 15:15:45 +0900

    화면 구성

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import FrontEnd from './pages/FrontEnd';
 
+
 function App() {
   return (
     <>
     <FrontEnd>
 
     </FrontEnd>
+
+
 </>
 
   );
--- src/pages/FrontEnd.js
+++ src/pages/FrontEnd.js
@@ -1,9 +1,10 @@
 import React from "react";
 import styled from "styled-components";
-
+import {Box, Grommet,Button, Button as GrommetButton} from 'grommet';
+import { TextField } from "material-ui";
 const StyledInput=styled.input`
-height: 20px;
-width: 250px;
+height: 36px;
+width: 806px;
 border-radius: 5px;
 font-size: 15px;
 border: 2px solid gray;
@@ -11,6 +12,95 @@ alignItems: 'center'
 justifyContent: 'center',
 `;
 
+const StyledButton = styled.button`
+  height: 36px;
+  width: 128px;
+  background-color: #EB5757;
+  font-size: 20px;
+  font-color: #FFFFFF;
+  border-radius: 5px;
+  font-size:15px;
+`;
+
+const StyledButton2 = styled.button`
+  height: 36px;
+  width: 128px;
+  background-color: #34B755;
+  font-size: 20px;
+  font-color: #FFFFFF;
+  border-radius: 5px;
+  font-size:15px;
+`;
+
+const StyledButton3 = styled.button`
+  height: 35px;
+  width: 964px;
+  background-color: #34B755;
+  font-size: 20px;
+  font-color: #FFFFFF;
+  border-radius: 5px;
+  font-size:15px;
+`;
+
+const StyledButton4 = styled.button`
+  height: 100px;
+  width: 100px;
+  background-color: #ffffff;
+  font-size: 20px;
+  font-color: #FFFFFF;
+  border-radius: 5px;
+  font-size:15px;
+`;
+const StyledButton5 = styled.button`
+  height: 35px;
+  width: 100px;
+  background-color: #124271;
+  font-size: 20px;
+  font-color: #FFFFFF;
+  border-radius: 5px;
+  font-size:15px;
+`;
+
+const theme = {
+  global: {
+    font: {
+      family: 'Roboto',
+      size: '14px',
+      height: '20px',
+    },
+  },
+};
+
+
+const AppBar=(props)=>(
+  <Box
+  tag='header'
+  direction='row'
+  // align='center'
+  justify='between'
+  background='white'
+  // pad={{vertical:'small',horizontal:'medium'}}
+  elevation='medium'
+  width='964px'
+  height='362px'
+  {...props}
+  />
+);
+
+const AppBar2=(props)=>(
+  <Box
+  tag='header'
+  direction='row'
+  // align='center'
+  justify='between'
+  background='white'
+  // pad={{vertical:'small',horizontal:'medium'}}
+  elevation='medium'
+  width='100px'
+  height='100px'
+  {...props}
+  />
+);
 
 function FrontEnd() {
 
@@ -21,11 +111,31 @@ function FrontEnd() {
 <h1>
     Front / Back / DB
 </h1>
-<a>당신의 이름</a>
+
+<AppBar2>
+  <StyledButton5>
+    Front-end
+  </StyledButton5>
+</AppBar2>
+&nbsp;&nbsp;&nbsp;&nbsp;
+<AppBar2>
+  <StyledButton5>
+    Back-end
+  </StyledButton5>
+</AppBar2>
+&nbsp;&nbsp;&nbsp;&nbsp;
+<AppBar2>
+  <StyledButton5>
+    DB
+  </StyledButton5>
+</AppBar2>
+<br/>
+<br/>
+<a>당신의 이름</a> 
 &nbsp;&nbsp;
 <StyledInput placeholder={`이름을 입력해주세요.`}/>
 &nbsp;&nbsp;
-<button>이름 확인</button>
+<StyledButton>이름 확인</StyledButton>
 
 <br/>
 <br/>
@@ -33,9 +143,20 @@ function FrontEnd() {
 &nbsp;&nbsp;
 <StyledInput placeholder={`아무 텍스트나 입력해주세요.`}/>
 &nbsp;&nbsp;
-<button>추가 버튼</button>
+<StyledButton2>추가 버튼</StyledButton2>
 
-<p>결과 화면</p>
+<br/>
+<br/>
+<br/>
+<Grommet theme={theme}></Grommet>
+<AppBar>
+
+ <Box direction='row'>
+
+ <StyledButton3>결과</StyledButton3>
+</Box>
+
+</AppBar>
 
     </div>
     </center>
