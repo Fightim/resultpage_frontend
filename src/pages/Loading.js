29b30ef61862561e62e2f98adb97338975b08bbf
EXAMPLE1ST <koo6250@naver.com>
2022-11-28 16:33:39 +0900

    overflow scroll

--- src/pages/Loading.js
+++ src/pages/Loading.js
@@ -1,7 +1,6 @@
 import {useNavigate, useParams } from "react-router-dom"
 import axios from "axios";
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect } from "react";
 
 
 export default function Loading() {
@@ -9,44 +8,44 @@ export default function Loading() {
     const params = useParams();
     const tokenkey = params.token;
     
-
-    axios.get(`${process.env.REACT_APP_URL}/instances/backend`, {
-    headers: {
-        Authorization: `Bearer ${tokenkey}`}
-    })
-    .then((response)=>{
-        console.log(response);
-        //nullable:true이면 ip 있는것(back이 있는 것), back 검사 후 rds검사하기 state로 reloading해주기
-        if (response.data!==""){
-            axios.get(`http://52.78.76.251/db`, {
-                headers: {
-                    Authorization: `Bearer ${tokenkey}`
+    useEffect(()=>{
+            axios.get(`${process.env.REACT_APP_URL}/instances/backend`, {
+            headers: {
+                Authorization: `Bearer ${tokenkey}`}
+            })
+            .then((response)=>{
+                console.log(response);
+                //nullable:true이면 ip 있는것(back이 있는 것), back 검사 후 rds검사하기 state로 reloading해주기
+                if (response.data!==""){
+                    axios.get(`http://52.78.76.251/db`, {
+                        headers: {
+                            Authorization: `Bearer ${tokenkey}`
+                        }
+                        }).then((response)=>{
+                            console.log(response);
+                            if (response.status===200){
+                                navigate("/PlusDB/" + tokenkey);
+                                //여기도 :token 이렇게해야하낭
+                            }
+                            else{
+                                navigate("/PlusBackend/" + tokenkey);
+                                 //여기에 rds 검사
+                            }
+                        }
+                        )
+               
+         
                 }
-                }).then((response)=>{
-                    console.log(response);
-                    if (response.status===200){
-                        navigate("/PlusDB/" + tokenkey);
-                        //여기도 :token 이렇게해야하낭
-                    }
-                    else{
-                        navigate("/PlusBackend/" + tokenkey);
-                         //여기에 rds 검사
-                    }
-                }
-                )
-       
- 
-        }
-      
-        else{
-            <Link to={"/Frontend/" + tokenkey}></Link>
-        }
-    })
+              
+                else{
+                    navigate("/FrontEnd/" + tokenkey);
+            }})
+    },[]);
     
 
    
 
 
-    return <div>Loading {tokenkey}</div>
+    return <div>Loading..... {tokenkey}</div>
 }
 
